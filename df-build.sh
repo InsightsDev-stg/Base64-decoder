f9c509cda62a45b6d6521bfe76b3101cfa5e610f
Insights Service User <insightservice@InsightsService>
2016-10-06 07:35:11 +0000

    Instrumented by Insights Service for uploading the artifacts

--- df-build.sh
+++ df-build.sh
@@ -1,3 +1,18 @@
+# /*"
+# * Copyright (C) 2008 Subscription Insights Portal authors"
+# *"
+# * Licensed under the Apache License, Version 2.0 (the "License");"
+# * you may not use this file except in compliance with the License."
+# * You may obtain a copy of the License at"
+# *"
+# *      http://www.apache.org/licenses/LICENSE-2.0"
+# *"
+# * Unless required by applicable law or agreed to in writing, software"
+# * distributed under the License is distributed on an "AS IS" BASIS,"
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
+# * See the License for the specific language governing permissions and"
+# * limitations under the License."
+# */"
 echo "Build is done by now. Copying dependencies..."
 mvn dependency:copy-dependencies || { echo "Error: maven dependency:copy-dependencies failed"; exit 1; }
 echo "Cleaning up existing zip if any..."
@@ -10,4 +25,4 @@ zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar) || { echo "Err
 zip build.zip $(git ls-files -o | grep -e target/classes) || { echo "Error: Zipping binaries failed"; exit 1; }
 zip testbuild.zip $(git ls-files -o | grep -e target/test-classes) || { echo "Error: Zipping test binaries failed"; exit 1; }
 echo "Zipping artifacts DONE!!!"
-# Modified by Insights Service at 2016-06-16 14:16:29.787339
+# Modified by Insights Service at 2016-10-06 07:34:52.238591
